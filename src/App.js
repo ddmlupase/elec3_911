9f62aaa4ae295af85f44e31915a208b69cebe086
Daniel David <146162731+ddmlupase@users.noreply.github.com>
2025-09-21 19:31:18 +0800

    feat: additional react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 import Nav from './nav';
-import Home from './pages/home';
-import Portfolio from './pages/portfolio';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Login from './pages/Login';
+import Calculator from './pages/Calculator';
 
-const NavBarLayout = () => {
-  return (
-    <>
-      <Nav />
-      <Outlet />
-    </>
-  );
-};
+const NavBarLayout = () => (
+  <>
+    <Nav />
+    <Outlet />
+  </>
+);
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route element={<NavBarLayout />}>
           <Route path="/home" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/calculator" element={<Calculator />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
