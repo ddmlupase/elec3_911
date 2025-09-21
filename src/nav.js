ac0ac7d9bb0f983a736cba02f1842a20d9a10d45
Daniel David <146162731+ddmlupase@users.noreply.github.com>
2025-09-21 19:31:53 +0800

    feat: additional react

--- src/nav.js
+++ src/nav.js
@@ -1,31 +1,31 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="light">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/home">Portfolio Project</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/home" activeclassname="active">Home</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/portfolio" activeclassname="active">Portfolio</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/about">About</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">Contact</NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div className="App">      
+      <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <NavLink class="nav-brand" to="/home">PORTFOLIO PROJ</NavLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+            <NavLink class="nav-link" to="/home" activeclassname="active">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink class="nav-link" to="/portfolio" activeclassname="active">Portfolio</NavLink>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search" />
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
   );
 }
 
