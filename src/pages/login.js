faf3c4e6be7cb772115e9849683fecd1e9acfc62
Daniel David <146162731+ddmlupase@users.noreply.github.com>
2025-09-21 19:30:09 +0800

    feat: ad

---
+++ src/pages/login.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from "react";  
+import { useNavigate } from "react-router-dom";
+
+function Login(){
+    const [username,setUsername]=useState("");
+    const [password,setPassword]=useState("");
+    const [title,setTitle]=useState("");
+    // placeholder for username and password values since we dont have database yet
+    const realusername = 'admin';
+    const realpassword = 'adpw';
+
+    const navigate=useNavigate();
+
+    useEffect(()=>{
+        setTitle(`Hello, ${username}`);
+    }, [username])
+
+    const handleLogin=(e)=>{
+        e.preventDefault();
+        if(username===realusername && password===realpassword){
+            alert("Logged In Successful");
+            navigate("/home");
+        }
+        else {
+            alert("Login Error");
+        }
+    }
+    
+    return(
+        <div>
+            <h1>Login Form</h1>
+            <h2>{title}</h2>
+            <form onSubmit={handleLogin}>
+                <div className="form-group">
+                    <label>Username:</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        value={username}
+                        required
+                        maxLength={20} 
+                        onChange={(e)=>setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="form-group mb-5">
+                    <label>Password:</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        value={password}
+                        required
+                        maxLength={20} 
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit" class="btn btn-danger">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
